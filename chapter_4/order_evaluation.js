f03acf616e826e29f17faf394ca0f0690e7bb22c
bimri <bimri@outlook.com>
2022-01-05 17:51:42 +0300

    Order of Evaluation

---
+++ chapter_4/order_evaluation.js
@@ -0,0 +1,15 @@
+"Order of Evaluation"
+
+/*
+Operator precedence and associativity specify the order in which operations are performed
+in a complex expression, but they do not specify the order in which the subexpressions
+are evaluated.
+*/
+w = x + y * z
+/*
+In the expression w = x + y * z, for example, the subexpression w is
+evaluated first, followed by x, y, and z. Then the values of y and z are multiplied,
+added to the value of x, and assigned to the variable or property specified by expression
+w.
+*/
+
