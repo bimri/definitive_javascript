a1184c44a21ebc3ec54e3cde21e5c315143b9a97
bimri <bimri@outlook.com>
2022-01-05 16:32:53 +0300

    Primary Expression

---
+++ chapter_4/primary_expressions.js
@@ -0,0 +1,26 @@
+/*
+The simplest expressions, known as primary expressions, are those that stand alone—
+they do not include any simpler expressions. 
+
+Primary expressions in JavaScript are
+constant or literal values, certain language keywords, and variable references.
+*/
+
+// Literals are constant values that are embedded directly in your program.
+1.23                // A number literal
+"hello"             // A string literal
+// /pattern/           // A regular expression literal
+
+
+// Some of JavaScript’s reserved words are primary expressions
+true                // Evalutes to the boolean true value
+false               // Evaluates to the boolean false value
+null                // Evaluates to the null value
+this                // Evaluates to the "current" object
+
+
+// Finally, the third type of primary expression is a reference to a variable, constant, or
+// property of the global object:
+i                   // Evaluates to the value of the variable i.
+sum                 // Evaluates to the value of the variable sum.
+undefined           // The value of the "undefined" property of the global object
