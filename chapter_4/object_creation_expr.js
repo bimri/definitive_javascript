864f22f0583af0cec270f922f09266e4b2ed7919
bimri <bimri@outlook.com>
2022-01-05 17:22:56 +0300

    Object Creation Expressions

---
+++ chapter_4/object_creation_expr.js
@@ -0,0 +1,17 @@
+"Object Creation Expressions"
+
+/*
+An object creation expression creates a new object and invokes a function (called a
+constructor) to initialize the properties of that object. Object creation expressions are
+like invocation expressions except that they are prefixed with the keyword new:
+*/
+new Object()
+new Point(2,3)
+
+
+/*
+If no arguments are passed to the constructor function in an object creation expression,
+the empty pair of parentheses can be omitted:
+*/
+new Object
+new Date 
