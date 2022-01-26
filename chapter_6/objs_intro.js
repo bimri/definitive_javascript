f9e7d35fea620a605f630c7f8c7304d94db5b58b
bimri <bimri@outlook.com>
2022-01-26 17:27:00 +0300

    6.1 Introduction to Objects

---
+++ chapter_6/objs_intro.js
@@ -0,0 +1,19 @@
+/*
+An object is a composite value: it aggregates multiple values (primitive values or
+other objects) and allows you to store and retrieve those values by name. An object is
+an unordered collection of properties, each of which has a name and a value.
+
+An object is a composite value: it aggregates multiple values (primitive values or
+other objects) and allows you to store and retrieve those values by name. An object is
+an unordered collection of properties, each of which has a name and a value.
+
+JavaScript objects are dynamic—properties can usually be added and deleted—but
+they can be used to simulate the static objects and “structs” of statically typed languages.
+
+Any value in JavaScript that is not a string, a number, a Symbol, or true, false, null,
+or undefined is an object. And even though strings, numbers, and booleans are not
+objects, they can behave like immutable objects.
+
+The most common things to do with objects are to create them and set, query, delete,
+test, and enumerate their properties.
+*/
