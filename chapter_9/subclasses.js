9e2c8cb12204fedae7221d77b88ff079d2755db6
bimri <bimri@outlook.com>
2022-03-17 01:43:05 +0300

    9.5 Subclasses

---
+++ chapter_9/subclasses.js
@@ -0,0 +1,34 @@
+// 9.5 Subclasses
+
+// 9.5.1 Subclasses and Prototypes
+// 9.5.2 Subclasses with extends and super
+// A trivial Array subclass that adds getters for the first and last elements.
+class EZArray extends Array {
+    get first() { return this[0]; }
+    get last() { return this[this.length-1]; }
+}
+
+
+let a = new EZArray();
+a instanceof EZArray                // => true: a is subclass instance
+a instanceof Array                  // => true: a is also a superclass instance.
+a.push(1,2,3,4);                    // a.length == 4; we can use inherited methods
+a.first                             // => 1: first getter defined by subclass
+a.last                              // => 3: last getter defined by subclass
+a[1]                                // => 2: regular array access syntax still works.
+Array.isArray(a)                    // => true: subclass instance really is an array
+EZArray.isArray(a)                  // => true: subclass inherits static methods, too!
+
+
+// EZArray inherits instance methods because EZArray.prototype
+// inherits from Array.prototype
+Array.prototype.isPrototypeOf(EZArray.prototype)        // => true
+
+// And EZArray inherits static methods and properties because
+// EZArray inherits from Array. This is a special feature of the
+// extends keyword and is not possible before ES6.
+Array.isPrototypeOf(EZArray)                            // => true
+
+
+
+// 9.5.4 Class Hierarchies and Abstract Classes
