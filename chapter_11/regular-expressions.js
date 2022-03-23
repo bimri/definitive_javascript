c51bef329a91c21ac0637e7e5e0a12893bf065c0
bimri <bimri@outlook.com>
2022-03-23 23:19:58 +0300

    11.3.2 String Methods for Pattern Matching

--- chapter_11/regular-expressions.js
+++ chapter_11/regular-expressions.js
@@ -36,9 +36,91 @@ however, explicitly define your own Unicode character classes; for example,
 /[\u0400-\u04FF]/ matches any one Cyrillic character.)
 */
 
-
 // Regular expression repetition characters
 let r = /\d{2,4}/;          // Match between two and four digits
 r = /\w{3}\d?/;             // Match exactly three word characters and optional digit
 r = /\s+java\s+/;           // Match "java" with one or more spaces before or after
 r = /[^(]*/;                // Match zero or more characters that are not open parens
+
+
+
+// 11.3.2 String Methods for Pattern Matching
+/* search() */
+"JavaScript".search(/script/ui)             // => 4
+"Python".search(/script/ui)                 // => -1
+
+/* replace() */
+// No matter how it is capitalized, replace it with the correct capitalization
+text.replace(/javascript/gi, "JavaScript");
+
+// A quote is a quotation mark, followed by any number of
+// nonquotation mark characters (which we capture), followed
+// by another quotation mark.
+let quote = /"([^"])"/g;
+// Replace the straight quotation marks with guillemets
+// leaving the quoted text (stored in $1) unchanged.
+'He said "stop"'.replace(quote, '«$1»')                 // => 'He said <<stop>>'
+
+/* If your RegExp uses named capture groups, then you can refer to the matching text
+by name rather than by number: */
+let quoted = /"(?<quotedText>[^"])"/g;
+'He said "stop"'.replace(quote, '«$<quotedText>»')      // => 'He said «stop»'
+
+/* uses a replacement function to convert decimal integers in a string to hexadecimal:*/
+let s = "15 times 15 is 225";
+s.replace(/\d+/gu, n => parseInt(n).toString(16))       // "f times f is e1"
+
+/* match() */
+"7 plus 8 equals 15".match(/\d+/g)                      // => ["7", "8", "15"]
+
+// A very simple URL parsing RegExp
+let url = /(\w+):\/\/([\w.]+)\/(\S*)/;
+let text = "Visit my blog at http://www.example.com/~david";
+let match = text.match(url);
+let fullurl, protocol, host, path;
+if (match !== null) {
+    fullurl = match[0];                                 // fullurl == "http://www.example.com/~david"
+    protocol = match[1];                                // protocol == "http"
+    host = match[2];                                    // host == "www.example.com"
+    path = match[3];                                    // path == "~david"
+}
+
+let url1 = /(?<protocol>\w+):\/\/(?<host>[\w.]+)\/(?<path>\S*)/;
+let text1 = "Visit my blog at http://www.example.com/~david";
+let match1 = text1.match(url1);
+match1[0]                                               // => "http://www.example.com/~david"
+match1.input                                            // => text
+match1.index                                            // => 17
+match1.groups.protocol                                  // => "http"
+match1.groups.host                                      // => "www.example.com"
+match1.groups.path                                      // => "~david"
+
+/* If the y flag is set without g, then match() tries to find a single match, and, by default,
+this match is constrained to the start of the string. */
+let vowel = /[aeiou]/y;                 // Sticky vowel match
+"test".match(vowel)                     // => null: "test" does not begin with a vowel
+vowel.lastIndex = 1;                    // => Specify a different match position
+"test".match(vowel)[0]                  // => "e": we founf a vowel at position 1
+vowel.lastIndex                         // => 2: lastIndex was automatically updated
+"test".match(vowel)                     // => null: no vowel at position 2
+vowel.lastIndex                         // => 0: lastIndex gets reset after failed match
+
+/* matchAll() */
+// One or more Unicode alphabetic characters between word boundaries
+const words = /\b\p{Alphabetic}+\b/gu;          // \p is not supported in firefox yet
+const texts = "This is a naïve test of the matchAll() method.";
+for (let word in texts.matchAll(words)) {
+    console.log(`Found '${word[0]}' at index ${word.index}.`);
+}
+
+/* split() */
+"123,456,789".split(",")                // => ["123", "456", "789"]
+
+// method can also take a regular expression as its argument split()
+"1, 2, 3,\n4, 5".split(/\s*,\s*/)       // => ["1", "2", "3", "4", "5"]
+
+/* Surprisingly, if you call split() with a RegExp delimiter and the regular expression
+includes capturing groups, then the text that matches the capturing groups will be
+included in the returned array. */
+const htmlTag = /<([^>]+)>/;            // < followed by one or more non->, followed by >
+"Testing<br/>1,2,3".split(htmlTag)      // => ["Testing", "br/", "1,2,3"]
