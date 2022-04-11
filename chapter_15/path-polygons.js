0f9abfa21705eda5cc830a957365639a56b59106
bimri <bimri@outlook.com>
2022-04-11 23:53:08 +0300

    graphics' titles edits

--- chapter_15/path-polygons.js
+++ chapter_15/path-polygons.js
@@ -1,3 +1,11 @@
+// 15.8.1 Paths and Polygons
+/* To draw lines on a canvas and to fill the areas enclosed by those lines, you begin by
+defining a path. A path is a sequence of one or more subpaths. A subpath is a
+sequence of two or more points connected by line segments. Begin a new path with the beginPath() 
+method. Begin a new subpath with the moveTo() method. Once you have established the starting point of a
+subpath with moveTo(), you can connect that point to a new point with a straight line
+by calling lineTo(). */
+
 // Define a regular polygon with n sides, centered at (x,y) with radius r.
 // The vertices are equally spaced along the circumference of a circle.
 // Put the first vertex straight up or at the specified angle.
@@ -33,3 +41,14 @@ c.lineWidth = 5;                                // five pixels wide.
 // Now draw all the polygons (each in its own subpath) with these calls
 c.fill();                                       // Fill the shapes
 c.stroke();                                     // And stroke their outlines
+
+
+/* For optimum image quality, you should not use the width and height attributes to
+set the on-screen size of the canvas. Instead, set the desired on-screen size CSS pixel
+size of the canvas with CSS width and height style attributes. Then, before you begin
+drawing in your JavaScript code, set the width and height properties of the canvas
+object to the number of CSS pixels times window.devicePixelRatio. */
+
+
+// SVG graphics, which remain crisp no matter the
+// on-screen size or zoom level.
