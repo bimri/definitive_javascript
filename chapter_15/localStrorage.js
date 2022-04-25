7cceb3cc27ea356c5c0bf0658732d91f1207ad62
bimri <bimri@outlook.com>
2022-04-26 02:33:35 +0300

    15.12.2 Cookies

--- chapter_15/localStrorage.js
+++ chapter_15/localStrorage.js
@@ -24,3 +24,40 @@ let lastRead = new Date(Date.parse(localStorage.lastRead));
 // JSON makes a convenient encoding for any primitive or data structure.
 localStorage.data = JSON.stringify(data);           // Encode and store
 let data = JSON.parse(localStorage.data);           // Retrieve and decode.
+
+
+// 15.12.2 Cookies
+// Return the document's cookoes as a Map Object.
+// Assum that cookie calues are encoded with encodeURIComponent().
+function getCookies() {
+    let cookies = new Map();                        // The object we will return
+    let all = document.cookie;                      // Get all cookies in one big string
+    let list = all.split(";");                      // Split into individual name/value pairs
+    for(let cookie of list) {                       // For each cookie in that list
+        if (!cookie.includes("=")) continue;        // Skip if there is no = sign
+        let p = cookie.indexOf("=");                // Find the fist = sign
+        let name = cookie.substring(0, p);          // Get cookie name
+        let value = cookie.substring(p+1);          // Get cookie value
+        value = decodeURIComponent(value);          // Decode the value
+        cookies.set(name, value);                   // Remember cookie name and value
+    }
+    return cookies;
+}
+
+// Storing cookies
+/* To associate a transient cookie value with the current document, simply set the
+cookie property to a name=value string. */
+document.cookie = `version=${encodeURIComponent(document.lastModified)}`;
+
+
+// Store the name/value pair as a cookie, encoding the value with
+// encodeURIComponent() in order to escape semicolons, commas, and spaces.
+// If daysToLive is a number, set the max-age attribute so that the cookie
+// expires after the specified number of days. Pass 0 to delete a cookie.
+function setCookie(name, value, daysToLive=null) {
+    let cookie = `${name}=${encodeURIComponent(value)}`;
+    if (daysToLive !==null) {
+        cookie += `; max-age=${daysToLive*60*60*24}`;
+    }
+    document.cookie = cookie;
+}
